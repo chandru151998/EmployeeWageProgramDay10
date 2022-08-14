5dbb0df1bba594d3bfdf344cc6449aaaeff5d560
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-08-14 10:06:24 +0530

    UC9

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program using Objects");
+            Console.WriteLine("Welcome to Employeewage program.");
+
+            EmpWageBuilder dMart = new EmpWageBuilder("Dmart", 20, 2, 10);
+            EmpWageBuilder reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
+
+            dMart.ComputeEmpWage();
+            Console.WriteLine(dMart.ToString());
+
+            reliance.ComputeEmpWage();
+            Console.WriteLine(reliance.ToString());
         }
     }
 }
